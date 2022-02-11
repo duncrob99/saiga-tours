f72b812e4ea819bfc5febd416e1f05c14d76136d
Duncan Robertson <duncrob99@gmail.com>
2022-02-11 16:09:32 +1100

    Made itinerary days collapsible.

--- main/static/js/itinerary-slider.js
+++ main/static/js/itinerary-slider.js
@@ -1,5 +1,4 @@
 let slideIndex = 1;
-showSlides(slideIndex, false, true);
 
 // Next/previous controls
 function plusSlides(n) {
@@ -70,13 +69,17 @@ function scrollToView(elem, shouldScrollToTop, margin) {
     }
 
     if (bbox.top <= windowBox.navbar + margin && bbox.bottom <= window.innerHeight - margin) {
-        if (shouldScrollToTop) {
+        if (bbox.height < window.innerHeight ? !shouldScrollToTop : shouldScrollToTop) {
             scrollTo(windowBox.left, windowBox.top + bbox.top - windowBox.navbar - margin);
         } else {
             scrollTo(windowBox.left, windowBox.top + (bbox.top + bbox.height - window.innerHeight + margin));
         }
     } else if (bbox.top + bbox.height >= window.innerHeight - margin && bbox.top >= windowBox.navbar + margin) {
-        scrollTo(windowBox.left, windowBox.top + bbox.top - windowBox.navbar - margin);
+        if (bbox.height > window.innerHeight) {
+            scrollTo(windowBox.left, windowBox.top + bbox.top - windowBox.navbar - margin);
+        } else {
+            scrollTo(windowBox.left, windowBox.top + (bbox.top + bbox.height - window.innerHeight + margin));
+        }
     }
 }
 
@@ -93,10 +96,48 @@ function setVerticalHeight() {
     document.querySelector('.slides').style.height = max_height + 15 + 'px';
 }
 
+function minimiseSlides() {
+    let slides = document.getElementsByClassName('mySlides');
+    let max_height = window.innerHeight * 0.8;
+    console.log('minimising')
+    for (let i = 0; i < slides.length; i++) {
+        let slide = slides[i];
+        let style = getComputedStyle(slide);
+        let height = parseInt(style.height);
+        if (height > max_height) {
+            slide.classList.add('compact');
+        }
+    }
+}
+
+function expandSlide(day) {
+    let slide = document.getElementById(`day-${day}`);
+    slide.classList.remove('compact');
+    slide.classList.add('expanded');
+    setVerticalHeight();
+    scrollToView(slide);
+}
+
+function contractSlide(day) {
+    let slide = document.getElementById(`day-${day}`);
+    slide.classList.add('compact');
+    slide.classList.remove('expanded');
+    setVerticalHeight();
+    scrollToView(slide);
+}
+
 window.addEventListener('resize', setVerticalHeight);
-window.addEventListener('load', setVerticalHeight);
+window.addEventListener('load', () => {
+    minimiseSlides();
+    setVerticalHeight();
+});
 document.querySelectorAll('.dot').forEach((el, index) => {
     el.addEventListener('click', () => {
         currentSlide(index + 1);
     })
 })
+document.querySelectorAll('.mySlides').forEach(el => {
+    el.addEventListener('input', setVerticalHeight);
+});
+
+showSlides(slideIndex, false, true);
