94c85eb5c68da300139db38e75de9f68d96d51b5
Duncan Robertson <duncrob99@gmail.com>
2022-03-04 00:31:57 +1100

    Made visibility be imported from main app. Removed some console.logs.

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -2,17 +2,13 @@ function minimise_images() {
     document.querySelectorAll(`img`).forEach(img => {
         let img_src;
         if (img.hasAttribute('src')) {
-            console.log('from src');
             img_src = img.getAttribute('src');
         } else if (img.hasAttribute('full-size-src')) {
-            console.log('from full-size-src');
             img_src = img.getAttribute('full-size-src');
         } else return;
-        console.log('orig src: ', img_src);
         if (img_src.startsWith('/static')) return;
         let img_size = `${Math.ceil(parseInt(getComputedStyle(img).width) * window.devicePixelRatio)}x${Math.ceil(parseInt(getComputedStyle(img).height) * window.devicePixelRatio)}`;
         img_src = img_src.replace(/^\/media\//, '').replaceAll(/\/resized-image\//g, '').replaceAll(/\/[0-9]+x[0-9]+\//g, '');
-        console.log('final src: ', `/resized-image/${img_src}/${img_size}/`);
         img.setAttribute('src', `/resized-image/${img_src}/${img_size}/`);
     })
 }
