663070dfb99605421fdd777a088b4d0b3d228607
Duncan Robertson <duncrob99@gmail.com>
2022-06-22 11:19:07 +0000

    Make sure both image size & src exist before changing src.

--- main/static/js/main.js
+++ main/static/js/main.js
@@ -2,6 +2,7 @@ function minimise_images() {
     document.querySelectorAll(`img`).forEach(img => {
         let img_src;
         if (img.hasAttribute('src')) {
+            console.log(img, 'still has src attribute');
             img_src = img.getAttribute('src');
         } else if (img.hasAttribute('full-size-src')) {
             img_src = img.getAttribute('full-size-src');
@@ -9,7 +10,9 @@ function minimise_images() {
         if (img_src.startsWith('/static')) return;
         let img_size = `${Math.ceil(parseInt(getComputedStyle(img).width) * window.devicePixelRatio)}x${Math.ceil(parseInt(getComputedStyle(img).height) * window.devicePixelRatio)}`;
         img_src = img_src.replace(/^\/media\//, '').replaceAll(/\/resized-image\//g, '').replaceAll(/\/[0-9]+x[0-9]+\//g, '');
-        img.setAttribute('src', `/resized-image/${img_src}/${img_size}/`);
+        if (img_src && img_size) {
+            img.setAttribute('src', `/resized-image/${img_src}/${img_size}/`);
+        }
     })
 }
 
