484e2fa949578eb271bfa93b85a139f157cae348
Duncan Robertson <duncrob99@gmail.com>
2022-06-24 14:35:17 +0100

    Made separators transition to their randomised position rather than jumping.

--- main/static/js/ckeditor_styling.js
+++ main/static/js/ckeditor_styling.js
@@ -1,3 +1,3 @@
 document.querySelectorAll('.separator path').forEach(sep => {
-    sep.style.transform = `translateX(-${Math.random()*50}px)`;
+    sep.style.transform = `translateX(${-Math.random()*50}px)`;
 });
