710d089c93127f29a1dd1665b529a1b9e0e07205
Duncan Robertson <duncrob99@gmail.com>
2024-06-26 23:21:14 +0700

    Added various static file maps to make whitenoise happy

--- travel_website/settings.py
+++ travel_website/settings.py
@@ -100,6 +100,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
     "django_unused_media",
